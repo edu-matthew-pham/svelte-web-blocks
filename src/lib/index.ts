7ea4d35ae17b9343742e83d7cdaed9147c694824
Matthew Pham <103876372+edu-matthew-pham@users.noreply.github.com>
2025-04-22 04:35:31 +1000

    Updated imports with V2 with refactored component modules

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -7,10 +7,26 @@ export { defaultToolbox } from './toolbox/default-toolbox.js';
 export { createBlocksFromJson } from './blocks/parser.js';
 export { convertHTMLToPug } from './blocks/pugTemplates.js';
 
+// Export new refactored modules
+export { createGenerator } from './utils/generator-factory.js';
+export { createBlockDefinitions } from './utils/block-factory.js';
+export { initializeBlocks } from './components/blocks/index.js';
+
+// Export component modules
+export * from './components/document/index.js';
+export * from './components/navigation/index.js';
+export * from './components/content/index.js';
+export * from './components/features/index.js';
+export * from './components/forms/index.js';
+export * from './components/dynamic/index.js';
+export * from './blocks/js/dom/index.js';
+
 import BlocklyWorkspace from './BlocklyWorkspace.svelte';
 import BlocklyWorkspaceWithPreview from './BlocklyWorkspaceWithPreview.svelte';
+import BlocklyEditor from '$lib/components/workspace/BlocklyEditor.svelte';
 
 export {
     BlocklyWorkspace,
-    BlocklyWorkspaceWithPreview
+    BlocklyWorkspaceWithPreview,
+    BlocklyEditor
 };
