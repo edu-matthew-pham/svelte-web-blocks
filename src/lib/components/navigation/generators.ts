2d5db68c02e383fd9767651a80f4d855176ae7de
Matthew Pham <103876372+edu-matthew-pham@users.noreply.github.com>
2025-04-29 19:08:01 +1000

    Added high level generation to navigation

--- src/lib/components/navigation/generators.ts
+++ src/lib/components/navigation/generators.ts
@@ -1,28 +1,72 @@
-import { createGenerator } from '$lib/utils/generator-factory.js';
 import * as HTML from '$lib/blocks/htmlTemplates.js';
+import * as Blockly from 'blockly';
+import { javascriptGenerator } from 'blockly/javascript';
 
-// Create navigation generators using the factory
+// Create navigation generators
 export const navigationGenerators = {
   // Header generator
-  web_header: createGenerator({
-    propertyMappings: [
-      { componentProp: 'logoText', blockField: 'LOGO_TEXT' },
-      { componentProp: 'includeSignup', blockField: 'INCLUDE_SIGNUP', transform: val => val === 'TRUE' }
-    ],
-    childInputs: [
-      { inputName: 'LINKS' }
-    ],
-    htmlRenderer: (props, children, attributes) => 
-      HTML.createHeaderHTML(props.logoText, props.includeSignup, children.links, attributes) + '\n'
-  }),
+  web_header: {
+    html: function(block: Blockly.Block) {
+      const logoText = block.getFieldValue('LOGO_TEXT');
+      const includeSignup = block.getFieldValue('INCLUDE_SIGNUP') === 'TRUE';
+      const navigationItems = javascriptGenerator.statementToCode(block, 'LINKS');
+      
+      // Get ID and CLASS values
+      const id = block.getFieldValue('ID') || '';
+      const className = block.getFieldValue('CLASS') || '';
+      
+      const attributes = { id, className };
+      
+      return HTML.createHeaderHTML(logoText, includeSignup, navigationItems, attributes) + '\n';
+    },
+    
+    highLevel: function(block: Blockly.Block) {
+      const logoText = block.getFieldValue('LOGO_TEXT');
+      const includeSignup = block.getFieldValue('INCLUDE_SIGNUP') === 'TRUE';
+      
+      // Process navigation items
+      const children = [];
+      let navBlock = block.getInputTargetBlock('LINKS');
+      while (navBlock) {
+        const navItem = javascriptGenerator.blockToHighLevel(navBlock);
+        if (navItem) children.push(navItem);
+        navBlock = navBlock.getNextBlock();
+      }
+      
+      return {
+        type: "header",
+        properties: {
+          logoText,
+          includeSignup
+        },
+        children: children
+      };
+    }
+  },
   
   // Navigation item generator
-  web_nav_item: createGenerator({
-    propertyMappings: [
-      { componentProp: 'text', blockField: 'TEXT' },
-      { componentProp: 'url', blockField: 'URL' }
-    ],
-    htmlRenderer: (props, children, attributes) => 
-      HTML.createNavItemHTML(props.text, props.url, attributes)
-  })
+  web_nav_item: {
+    html: function(block: Blockly.Block) {
+      const text = block.getFieldValue('TEXT');
+      const url = block.getFieldValue('URL');
+      
+      // Get ID and CLASS values
+      const id = block.getFieldValue('ID') || '';
+      const className = block.getFieldValue('CLASS') || '';
+      
+      const attributes = { id, className };
+      
+      return HTML.createNavItemHTML(text, url, attributes);
+    },
+    
+    highLevel: function(block: Blockly.Block) {
+      return {
+        type: "navItem",
+        properties: {
+          text: block.getFieldValue('TEXT'),
+          url: block.getFieldValue('URL')
+        }
+      };
+    }
+  }
 }; 
