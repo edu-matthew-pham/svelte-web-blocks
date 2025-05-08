bd7a130c16b69cc8c8378655c0354aea782414db
Matthew Pham <103876372+edu-matthew-pham@users.noreply.github.com>
2025-05-08 20:13:03 +1000

    Updated toolbox with easier to test if that makes use of variables_set

--- src/lib/components/js/logic/toolbox.js
+++ src/lib/components/js/logic/toolbox.js
@@ -4,6 +4,32 @@
 export const toolboxXml = `
  <category name="Logic" colour="210">
 
+    
+
+    <block type="variables_set">
+      <value name="VALUE">
+        <block type="text">
+          <field name="TEXT">abc</field>
+        </block>
+      </value>
+      <next>
+            <block type="controls_if">
+      <value name="IF0">
+        <block type="expression_compare">
+          <field name="EXPRESSION">item == "abc"</field>
+        </block>
+      </value>
+      <statement name="DO0">
+        <block type="console_log">
+          <value name="TEXT">
+            <block type="text">
+              <field name="TEXT">item is abc</field>
+            </block>
+          </value>
+        </block>
+      </next>
+    </block>
+
     <block type="controls_if">
       <value name="IF0">
         <block type="expression_compare">
@@ -22,60 +48,7 @@ export const toolboxXml = `
     </block>
 
     <block type="expression_compare">
-      <field name="EXPRESSION">x > 0 && y < 10</field>
+      <field name="EXPRESSION">1==1 && 1<=2</field>
     </block>
 
-    <block type="logic_operation">
-      <field name="OP">AND</field>
-      <value name="A">
-        <block type="logic_boolean">
-          <field name="BOOL">TRUE</field>
-        </block>
-      </value>
-      <value name="B">
-        <block type="logic_boolean">
-          <field name="BOOL">TRUE</field>
-        </block>
-      </value>
-    </block>
-
-    <block type="logic_negate">
-      <value name="BOOL">
-        <block type="logic_boolean">
-          <field name="BOOL">FALSE</field>
-        </block>
-      </value>
-    </block>
-
-    <block type="logic_ternary">
-      <value name="IF">
-        <block type="logic_boolean">
-          <field name="BOOL">TRUE</field>
-        </block>
-      </value>
-      <value name="THEN">
-        <block type="text">
-          <field name="TEXT">yes</field>
-        </block>
-      </value>
-      <value name="ELSE">
-        <block type="text">
-          <field name="TEXT">no</field>
-        </block>
-      </value>
-    </block>
-
-    <block type="controls_if"></block>
-    <block type="logic_compare">
-      <field name="OP">EQ</field>
-    </block>
-    <block type="logic_operation">
-      <field name="OP">AND</field>
-    </block>
-    <block type="logic_negate"></block>
-    <block type="logic_boolean">
-      <field name="BOOL">TRUE</field>
-    </block>
-    <block type="logic_null"></block>
-    <block type="logic_ternary"></block>
 </category>`; 
