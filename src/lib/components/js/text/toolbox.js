231b14e7434847b42677c3a299062c49ff7df672
Matthew Pham <103876372+edu-matthew-pham@users.noreply.github.com>
2025-05-09 20:49:19 +1000

    Updated high level for lists split

--- src/lib/components/js/text/toolbox.js
+++ src/lib/components/js/text/toolbox.js
@@ -68,12 +68,111 @@ export const toolboxXml = `<category name="Text" colour="160">
                     </block>
                   </value>
                   <next>
-                    <block type="console_log">
-                      <value name="TEXT">
-                        <block type="text">
-                          <field name="TEXT">Text operations complete!</field>
+                    <block type="variables_set">
+                      <field name="VAR">character</field>
+                      <value name="VALUE">
+                        <block type="text_charAt">
+                          <mutation at="true"></mutation>
+                          <field name="WHERE">FROM_START</field>
+                          <value name="VALUE">
+                            <block type="text">
+                              <field name="TEXT">Hello World</field>
+                            </block>
+                          </value>
+                          <value name="AT">
+                            <block type="math_number">
+                              <field name="NUM">1</field>
+                            </block>
+                          </value>
                         </block>
                       </value>
+                      <next>
+                        <block type="variables_set">
+                          <field name="VAR">substring</field>
+                          <value name="VALUE">
+                            <block type="text_getSubstring">
+                              <mutation at1="true" at2="true"></mutation>
+                              <field name="WHERE1">FROM_START</field>
+                              <field name="WHERE2">FROM_START</field>
+                              <value name="STRING">
+                                <block type="text">
+                                  <field name="TEXT">Hello World</field>
+                                </block>
+                              </value>
+                              <value name="AT1">
+                                <block type="math_number">
+                                  <field name="NUM">1</field>
+                                </block>
+                              </value>
+                              <value name="AT2">
+                                <block type="math_number">
+                                  <field name="NUM">5</field>
+                                </block>
+                              </value>
+                            </block>
+                          </value>
+                          <next>
+                            <block type="variables_set">
+                              <field name="VAR">upperCase</field>
+                              <value name="VALUE">
+                                <block type="text_changeCase">
+                                  <field name="CASE">UPPERCASE</field>
+                                  <value name="TEXT">
+                                    <block type="text">
+                                      <field name="TEXT">Hello World</field>
+                                    </block>
+                                  </value>
+                                </block>
+                              </value>
+                              <next>
+                                <block type="variables_set">
+                                  <field name="VAR">trimmedText</field>
+                                  <value name="VALUE">
+                                    <block type="text_trim">
+                                      <field name="MODE">BOTH</field>
+                                      <value name="TEXT">
+                                        <block type="text">
+                                          <field name="TEXT">  Hello World  </field>
+                                        </block>
+                                      </value>
+                                    </block>
+                                  </value>
+                                  <next>
+                                    <block type="variables_set">
+                                      <field name="VAR">splitText</field>
+                                      <value name="VALUE">
+                                        <block type="lists_split">
+                                          <mutation mode="SPLIT"></mutation>
+                                          <field name="MODE">SPLIT</field>
+                                          <value name="INPUT">
+                                            <block type="text">
+                                              <field name="TEXT">Hello,World,Example</field>
+                                            </block>
+                                          </value>
+                                          <value name="DELIM">
+                                            <block type="text">
+                                              <field name="TEXT">,</field>
+                                            </block>
+                                          </value>
+                                        </block>
+                                      </value>
+                                      <next>
+                                        <block type="console_log">
+                                            <value name="TEXT">
+                                            <block type="text">
+                                                <field name="TEXT">Text operations complete!</field>
+                                            </block>
+                                            </value>
+                                        </block>
+                                      </next>
+                                    </block>
+                                  </next>
+                                </block>
+                              </next>
+                            </block>
+                          </next>
+                        </block>
+                      </next>
                     </block>
                   </next>
                 </block>
